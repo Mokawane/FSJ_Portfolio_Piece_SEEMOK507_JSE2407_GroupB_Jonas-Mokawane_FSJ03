144b8395ff33e29f87313cd56dcdb0d2567cfeae
Jonas <jonas.mokawane98@gmail.com>
2024-10-10 15:06:21 +0200

    Remove auth.js file and AuthProvider import from layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import "./globals.css";
 import Header from "../components/Header";
-import { AuthProvider } from "@/context/AuthContext";
 
 export default function RootLayout({ children }) {
   return (
